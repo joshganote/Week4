e8bf24dd3514b9998d06c82410115861bea7c46b
Josh Ganote <j.ganote0825@gmail.com>
2019-10-28 01:06:34 -0500

    Added Stretch Goals

--- scripts/week4.js
+++ scripts/week4.js
@@ -1,9 +1,9 @@
 console.log('js');
 
-let parkedCars = []
+let parkedCars = [ 'PT Cruiser', 'Mustang', 'WRX STI', 'Firebird', 'Camero', 'Charger' ]
 const maxCars = 7;
 
-function parkCar( carName ){
+function parkCar( carName, color ){
   console.log( 'room for parkCar' );
   parkedCars.push( carName )
   if( maxCars >= parkedCars.length ){
@@ -14,4 +14,35 @@ function parkCar( carName ){
   } // end no room
 } // end of parkCar
 
+parkCar( 'PT Cruiser', 'Red' );
+parkCar( 'Mustang', 'Blue' );
+parkCar( 'WRX STI', 'Black' );
+parkCar( 'Firebird', 'Orange' );
+parkCar( 'Camero', 'Silver' );
+parkCar( 'Charger', 'Purple' );
+
 console.log( parkCar() );
+
+// Stretch Goals
+// 1.
+function isParkedHere( carName ){
+  if( isParkedHere() = carName ){
+    return true;
+  } // end correct inventory
+  else{
+    return false;
+  } // end not correct inventory
+} // end of isParkedHere
+isParkedHere( 'Porsche 911 GT3' );
+console.log( 'Porsche 911 GT3', isParkedHere() );
+
+// 2.
+function takeForASpin( carName, color ){
+  parkedCars.splice(0, 1);
+  if( takeForASpin() >=carName ){
+    return true;
+  }
+  else{
+    return false
+  }
+}
